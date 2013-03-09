459251d6faf17a27663ff998c3808fbd100fb52f
Chris <chris@chris-hp-ubuntu.(none)>
2013-03-09 11:26:35 +1100

    clean rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyshop::Application.config.secret_token = 'dda2252a4728453fdf063e6928e25b787e7ec72d5dacbd516a81be4e68543c782061410511824c59cf9e3e9d3b1ac4ef2babfbabd4fec4b97d14de9b36bc639a'
