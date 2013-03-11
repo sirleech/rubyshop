335436415286f7c0fe200d095a0bbffd6942d56e
Chris <chris@chris-hp-ubuntu.(none)>
2013-03-11 16:50:40 +1100

    add email to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  attr_accessible :username
+  attr_accessible :username, :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130310220555) do
+ActiveRecord::Schema.define(:version => 20130310235946) do
 
   create_table "products", :force => true do |t|
     t.string   "sku"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20130310220555) do
     t.string   "username"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
 end
